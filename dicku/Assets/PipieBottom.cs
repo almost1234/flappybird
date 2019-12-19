1dfc7421f4d7c2a740fe975c4bb36f9931343a7c
Steven Cungwanara <stevencung@yahoo.com>
2019-12-19 14:06:49 +0800

    no message

---
+++ dicku/Assets/PipieBottom.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipieBottom : MonoBehaviour
+{
+    private bool scoreGiven;
+    private int gameScore;
+    private DickBird idksomename;
+    public Transform topPipe;
+    public Transform bottomPipe;
+    private float numberHolder;
+    
+
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        idksomename = FindObjectOfType<DickBird>();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {   
+        topPipe.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
+        bottomPipe.Translate(new Vector3(-1, 0, 0) * Time.deltaTime);
+        if (topPipe.localPosition.x < -17.5f)
+        {
+            numberHolder = Random.Range(2.0f, 6.0f);
+            topPipe.SetPositionAndRotation(new Vector3(9.5f, numberHolder, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+            bottomPipe.SetPositionAndRotation(new Vector3(9.5f, numberHolder - 8, 0), Quaternion.Euler(new Vector3(0, 0, 0)));
+            scoreGiven = false;
+        }
+
+        if (topPipe.localPosition.x < -10.0f && scoreGiven == false)
+        {
+            gameScore = idksomename.Something();
+            scoreGiven = true;
+            Debug.Log(gameScore);
+        }
+
+
+    }
+}
---
+++ dicku/Assets/Script/DickBird.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DickBird : MonoBehaviour
+{
+    
+    private Rigidbody2D rb2D;
+    private float thrust = 300.0f;
+    public int score = 0;
+    public GameObject Pipes;
+    public int pipeGenerated;
+    public float timePassed;
+    public bool pipeFormed;
+
+
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        transform.position = new Vector3(0.0f, -2.0f, 0.0f);
+        pipeGenerated = 0;
+        pipeFormed = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb2D.AddForce(transform.up * thrust);
+            Debug.Log(Mathf.Round(timePassed % 2.0f));
+        }
+        
+    }
+
+    private void FixedUpdate()
+    { 
+       
+        if (Mathf.Round(timePassed % 2.0f) == 1 & pipeFormed == false & pipeGenerated < 6)
+        {
+            
+            Instantiate(Pipes, new Vector3(9.04f, Random.Range(4.0f,0.0f), 0), Quaternion.identity);
+            pipeGenerated = pipeGenerated + 1;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D  collision)
+    {
+        Debug.Log("Touched");
+        if(collision.gameObject.tag == "Pipe" || collision.gameObject.tag == "Floor"){
+            Time.timeScale = 0;
+        }
+    }
+
+    public int Something()
+    {
+        score = score + 1;
+        return score; 
+    }
+
+
+}
